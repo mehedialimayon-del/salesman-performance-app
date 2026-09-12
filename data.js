26e91620e5638e1260f3bb77283af5c128dafa86
mehedialimayon-del <mehedialimayon@gmail.com>
2026-09-12 14:55:55 +0800

    Add data.js with product and user information
    This commit introduces a new data.js file containing product categories, user information, salary rules, and outlet details for the application.

---
+++ data.js
@@ -0,0 +1,533 @@
+const CATEGORY_PRODUCTS = {
+  "KK Supermart": [
+    "PRAN Basil Seed Fruit Cocktail 290ml",
+    "PRAN Basil Seed Mango 290ml",
+    "PRAN Basil Seed Strawberry 290ml",
+    "PRAN Apple Juice 250ml TP",
+    "PRAN Fruit Cocktail 250ml TP",
+    "PRAN Mango Juice 250ml TP",
+    "PRAN Orange Juice 250ml TP",
+    "PRAN Mango Juice 330ml PET Bottle",
+    "PRAN Orange Juice 330ml PET Bottle",
+    "PRAN Mango Juice 1L PET Bottle",
+    "PRAN Orange Juice 1L PET Bottle",
+    "PRAN Tamarind Drink PET 320ml",
+    "PRAN Sour Plum Drink PET 320ml",
+    "PRAN Soya Milk Drink PET 320ml",
+    "Ice Tea Lemon 500ml",
+    "Power Extra Strong Drink 250ml",
+    "PRAN Power Can 250ml",
+    "PRAN Coconut Water Can 330ml",
+    "PRAN Lassi Yogurt 200ml PET Bottle",
+    "PRAN Lassi Mango 200ml PET Bottle",
+    "PRAN Lassi Strawberry 200ml PET Bottle",
+    "Fruit Fun Orange Cream Biscuit 35g",
+    "Fruit Fun Pineapple Cream Biscuit 35g",
+    "Fruit Fun Strawberry Cream Biscuit 35g",
+    "PRAN Butter Delight Biscuit 70g",
+    "PRAN Pista Almond Biscuit 70g",
+    "PRAN Potata Spicy Biscuit 100g",
+    "Black Forest Chocolate Central Fill Cookies 96g",
+    "Bisk Club Shero Cream Biscuits 74g",
+    "Bisk Club Shero Cream Biscuits 160g",
+    "Bisk Club Strawberry Cream Biscuit 90g",
+    "Bisk Club Pineapple Cream Biscuit 90g",
+    "Bisk Club Chocolate Cream Biscuit 90g",
+    "PRAN Delight Toast 250g",
+    "Mama Cube Wafer Chocolate 100g",
+    "Mama Cube Wafer Strawberry 100g",
+    "Mama Cube Wafer Milk 100g",
+    "PRAN Mustard Oil 200ml",
+    "PRAN BBQ Chanachur 290g",
+    "PRAN Hot Chanachur 150g",
+    "PRAN Puffed Rice 400g",
+    "PRAN Potato Crackers 60g",
+    "PRAN Potato Crackers Tomato 60g",
+    "PRAN Potato Crackers Cheese 60g",
+    "PRAN Pluto Choco Bean Tube 35g",
+    "PRAN Humpty Toy Chocolate 20g",
+    "PRAN Mega Lollipop",
+    "PRAN Choco Lord Bar 55g",
+    "PRAN Choco Chocolate Paste",
+    "RETAJ Natural Honey 250ml",
+    "RETAJ Natural Honey 500ml",
+    "Mr. Noodles Curry Cup 60g",
+    "Mr. Noodles Chicken Cup 60g",
+    "Mr. Noodles Vegetable Cup 60g",
+    "PRAN Korean Super Spicy Noodles 60g x5",
+    "Mr. Noodles Chicken 70g x5",
+    "Mr. Noodles Curry 70g x5",
+    "Wai Wai Noodles Chicken 5x75g",
+    "Wai Wai Noodles Vegetarian 5x75g",
+    "Barbican Pomegranate 330ml",
+    "Barbican Apple 330ml",
+    "Barbican Strawberry 330ml",
+    "Barbican Pineapple 330ml",
+    "Barbican Peach 330ml",
+    "Barbican Raspberry 330ml"
+  ],
+  "The Store": [
+    "Bisk Club Dry Cake 300g",
+    "Bisk Club Rusk Kit 330g",
+    "All Time Roma ChocoVanilla Cookies 70g",
+    "PRAN Black Forest Chocolate Cookies 70g",
+    "Lollipop Variety 10g",
+    "Mega Lollipop 8g",
+    "Treat Eclair Moon Shape 280g",
+    "PRAN Choco Lord Bar 55g",
+    "PRAN Hot Chanachur 130g",
+    "Chilli Powder 250g",
+    "Coriander Powder 250g",
+    "PRAN Meat Curry 250g Pouch",
+    "Turmeric Powder 250g",
+    "PRAN Curry Powder 250g",
+    "PRAN Basil Seed Mango 290ml",
+    "PRAN Basil Seed Fruit Cocktail 290ml",
+    "PRAN Basil Seed Strawberry 290ml",
+    "PRAN Basil Seed Litchi 290ml",
+    "PRAN Basil Seed Kiwi 290ml",
+    "PRAN Active Original 100ml",
+    "PRAN Active Strawberry 100ml",
+    "PRAN Active Green Apple 100ml",
+    "PRAN Lassi Yogurt 200ml",
+    "PRAN Lassi Mango 200ml",
+    "PRAN Lassi Strawberry 200ml",
+    "Shezan Mango Juice 250ml Glass Bottle",
+    "Shezan Lychee Juice Drink 250ml Glass Bottle",
+    "Shezan Pomegranate Juice 250ml Glass Bottle",
+    "PRAN Jus Apple 1000ml PET",
+    "PRAN Jus Mangga 1000ml PET",
+    "PRAN Jus Oren 1000ml PET",
+    "PRAN Jus Fruit Cocktail 1000ml PET",
+    "PRAN Jus Orange Value Pack 1.5L x2",
+    "PRAN Jus Apple Value Pack 1.5L x2",
+    "PRAN Jus Mango Value Pack 1.5L x2",
+    "PRAN Puffed Rice 400g",
+    "PRAN Potato Crackers 60g",
+    "PRAN Potato Crackers Tomato 60g",
+    "Mr. Noodles Vegetable 60g Cup",
+    "Mr. Noodles Chicken 60g Cup",
+    "Mr. Noodles Curry 60g Cup",
+    "PRAN Crunchy Wafer Orange 150g",
+    "PRAN Crunchy Wafer Choco 150g",
+    "PRAN Crunchy Wafer Strawberry 150g",
+    "PRAN Crunchy Wafer Milk 150g",
+    "PRAN Noodles Spicy Chicken 70g x5",
+    "PRAN Noodles Chicken Curry 70g x5",
+    "PRAN Noodles Korean Super Spicy 60g x5",
+    "Shezan Mango Juice 1000ml TP",
+    "Shezan Apple Juice 1000ml TP",
+    "Shezan Fruit Punch Juice 1000ml TP",
+    "PRAN Butter Cookies Biscuit 70g Combo",
+    "PRAN Peanut Biscuit 70g Combo",
+    "PRAN Jeera Biscuit 70g Combo",
+    "PRAN Pista Almond Biscuit 70g Combo",
+    "PRAN Cashew Nut Biscuit 70g Combo",
+    "Mughal Mustard Oil 200ml",
+    "FIZZE Blackcurrant Carbonated Drink 275ml",
+    "FIZZE Mango Peach Carbonated Drink 275ml",
+    "FIZZE Pineapple Coconut Carbonated Drink 275ml",
+    "FIZZE Strawberry Carbonated Drink 275ml",
+    "Fresh MOO Green Apple Flavored Drink 80ml",
+    "Fresh MOO Mango Flavored Drink 80ml",
+    "Fresh MOO Strawberry Flavored Drink 80ml",
+    "Fresh MOO Yogurt Flavored Drink 80ml",
+    "Mr. Noodles Chicken 70g x5 Combo",
+    "Mr. Noodles Curry 70g x5 Combo",
+    "Mr. Noodles Chicken Cup 40g",
+    "Mr. Noodles Curry Cup 40g"
+  ],
+  "TF Value Mart": [
+    "PRAN Puffed Rice",
+    "Humty Dumty",
+    "Bisk Club Chocolate 90g",
+    "Bisk Club Orange 90g",
+    "Bisk Club Pineapple 90g",
+    "Bisk Club Strawberry 90g",
+    "PRAN Basil Seed Cocktail 290ml",
+    "PRAN Basil Seed Mango 290ml",
+    "PRAN Basil Seed Orange 290ml",
+    "PRAN Basil Seed Pineapple 290ml",
+    "PRAN Basil Seed Strawberry 290ml",
+    "PRAN TP Juice 1L Guava",
+    "PRAN TP Juice 1L Pineapple",
+    "PRAN PET 330ml Apple",
+    "PRAN PET 330ml Mango",
+    "PRAN Juice Value Pack Assorted",
+    "PRAN Mango Juice 1.5L x2 1TR",
+    "PRAN PET 1LTR Mango"
+  ],
+  "Aneka": [
+    "Biskclub Rusk Kit 330g",
+    "PRAN Saltino Cookies 130g",
+    "PRAN Cream Filled Cup Cake Vanilla",
+    "PRAN Cream Filled Cup Cake Chocolate",
+    "PRAN Chanachur Normal 120g",
+    "Chanachur Hot 120g",
+    "Power Energy Drink Can 250ml",
+    "PRAN Basil Seed Mango 290ml",
+    "PRAN Basil Seed Cocktail 290ml",
+    "PRAN Basil Seed Strawberry 290ml",
+    "PRAN Basil Seed Litchi 290ml",
+    "PRAN Basil Seed Kiwi 290ml",
+    "Jus Apple PET Bottle 1000ml",
+    "Jus Mangga PET 1000ml",
+    "Jus Mangga Value Pack 1000ml",
+    "Puffed Rice 400g",
+    "Shero Roll Wafer Chocolate",
+    "Shero Roll Wafer Vanilla",
+    "Apple Juice TP 1000ml"
+  ],
+  "NSK Grocer": [
+    "PRAN Basil Seed Fruit Cocktail 290ml",
+    "PRAN Basil Seed Kiwi 290ml",
+    "PRAN Basil Seed Lychee 290ml",
+    "PRAN Basil Seed Mango 290ml",
+    "PRAN Basil Seed Pomegranate 290ml",
+    "PRAN Basil Seed Strawberry 290ml",
+    "PRAN Mango Juice 330ml",
+    "PRAN Orange Juice 330ml",
+    "PRAN Apple Juice 330ml",
+    "PRAN Jus Mangga 1L",
+    "PRAN Jus Oren 1L",
+    "PRAN Jus Apple 1L",
+    "PRAN Jus Fruit Cocktail 1L",
+    "PRAN Jus Mango Value Pack 2x1.5L",
+    "PRAN Jus Apple Value Pack 2x1.5L",
+    "PRAN Jus Orange Value Pack 2x1.5L",
+    "PRAN Latina Pink Guava 330ml",
+    "PRAN Latina Pink Mango 330ml",
+    "PRAN Latina Pink Pomegranate 330ml",
+    "Bisk Lub Rusk Kit 330g",
+    "PRAN Pista Almond Cookies Biscuit 70g",
+    "PRAN Butter Delight Cookies Biscuit 70g",
+    "PRAN Cashew Cookies Biscuit 70g",
+    "PRAN Puffed Rice 400g",
+    "PRAN Potato Crackers Tomato 60g",
+    "PRAN Potato Crackers 60g",
+    "Mr. Noodles Asam Laksa 70g",
+    "Mr. Noodles Chicken 70g",
+    "Mr. Noodles Curry 70g",
+    "Mr. Noodles Curry 70g",
+    "Mr. Noodles Tom Yum 70g",
+    "PRAN Korean Super Spicy Noodles 60g",
+    "PRAN Mr. Noodles BBQ Chicken 85g",
+    "PRAN Noodles Cheese 85g",
+    "PRAN Noodles Hot & Spicy 85g",
+    "PRAN Noodles Kimchi 85g",
+    "PRAN Noodles Super Spicy 85g",
+    "PRAN Mr. Noodles Beef Paper Cup 60g",
+    "PRAN Mr. Noodles Curry Paper Cup 60g",
+    "PRAN Mr. Noodles Vegetable Paper Cup 60g",
+    "PRAN Mr. Noodles Chicken Paper Cup 60g",
+    "PRAN Korean Noodles BBQ Chicken Paper Cup 60g",
+    "PRAN Korean Noodles Beef Paper Cup 60g",
+    "PRAN Korean Noodles Cheese Paper Cup 60g",
+    "PRAN Korean Noodles Hot & Spicy Paper Cup 60g",
+    "PRAN Korean Noodles Kimchi Paper Cup 60g",
+    "PRAN Korean Noodles Lamb Paper Cup 60g",
+    "PRAN Korean Noodles Super Spicy Paper Cup 60g"
+  ],
+  "Giant Hypermarket": [
+    "PRAN Basil Seed Mango 290ml",
+    "PRAN Basil Seed Strawberry 290ml",
+    "PRAN Basil Seed Litchi 290ml",
+    "PRAN Basil Seed Kiwi 290ml",
+    "Jus Apple 1000ml PET Bottle",
+    "Jus Mangga 1000ml PET",
+    "Value Pack 1000ml",
+    "Apple Juice Value Pack 1500ml x2",
+    "Orange Juice Value Pack 1500ml x2",
+    "Value Pack 1500ml x2",
+    "Bisk Club Cream Biscuit Orange 90g",
+    "Bisk Club Cream Biscuit Pineapple 90g",
+    "Bisk Club Cream Biscuit Chocolate 90g",
+    "Bisk Club Cream Biscuit Strawberry 90g",
+    "Potata Spicy Biscuit 100g",
+    "PRAN Crunchy Wafer Orange 150g",
+    "PRAN Crunchy Wafer Choco 150g",
+    "PRAN Crunchy Wafer Strawberry 150g",
+    "PRAN Crunchy Wafer Milk 150g"
+  ]
+};
+
+const ALL_PRODUCTS = [...new Set(Object.values(CATEGORY_PRODUCTS).flat())];
+
+window.APP_DATA = {
+  users: [
+    {
+      id: "M22075",
+      name: "MOHAMMAD BODRUDDAZA EMON",
+      role: "SR",
+      target: 85000
+    },
+    {
+      id: "M22268",
+      name: "MD LIMON MAJUMDER",
+      role: "SR",
+      target: 65000
+    },
+    {
+      id: "M22328",
+      name: "MD MUNNAF ALI",
+      role: "SR",
+      target: 65000
+    },
+    {
+      id: "M21954",
+      name: "MEHEDI ALIM AYON",
+      role: "MANAGER+SR",
+      target: 100000
+    }
+  ],
+
+  salaryRules: {
+    basic: 1700,
+    fuel: 300,
+    houseRent: 250,
+    foodThreshold: 50000,
+    foodHigh: 250,
+    foodLow: 100,
+    zeroSalesHigh: 200,
+    zeroSalesLow: 100,
+    commissionBelow80: 0.005,
+    commission80to100: 0.01,
+    commissionOverExtra: 0.02,
+    minMonthlyTarget: 50000
+  },
+
+  categoryProducts: CATEGORY_PRODUCTS,
+  products: ALL_PRODUCTS,
+
+  outlets: {
+    M22075: [],
+    M22268: [],
+
+    M22328: [
+      {
+        code: "5007781652",
+        name: "ECONSAVE EXPRESS DESA TASIK",
+        category: "Econsave",
+        totalSku: 8
+      },
+      {
+        code: "5000068963",
+        name: "Econsave Express Taman Midah",
+        category: "Econsave",
+        totalSku: 8
+      },
+      {
+        code: "5000068972",
+        name: "Econsave Express Taman Rasmi",
+        category: "Econsave",
+        totalSku: 8
+      },
+      {
+        code: "885668755",
+        name: "jaya Grocer Mont Kiara",
+        category: "Jaya Grocer",
+        totalSku: 10
+      },
+      {
+        code: "5007777457",
+        name: "KK SUPERMART & SUPERSTORE -JSE3",
+        category: "KK Supermart",
+        totalSku: 75
+      },
+      {
+        code: "5000076344",
+        name: "KK SUPERMART & SUPERSTORE SDN BHD - SHD2",
+        category: "KK Supermart",
+        totalSku: 75
+      },
+      {
+        code: "885664313",
+        name: "KK SUPERMART & SUPERSTORE SDN BHD(CHH-SP2)-CITY VILLA, JALAN HAJI HUSSEIN",
+        category: "KK Supermart",
+        totalSku: 75
+      },
+      {
+        code: "885664280",
+        name: "KK SUPERMART & SUPERSTORE SDN BHD(CKP-SP2)-CHOW KIT PUTRA",
+        category: "KK Supermart",
+        totalSku: 75
+      },
+      {
+        code: "885664272",
+        name: "KK SUPERMART & SUPERSTORE SDN BHD(JIP2-SP2)-JALAN IPOH 2",
+        category: "KK Supermart",
+        totalSku: 75
+      },
+      {
+        code: "885664361",
+        name: "KK SUPERMART & SUPERSTORE SDN BHD(JIP3-SP2)-JALAN IPOH 3",
+        category: "KK Supermart",
+        totalSku: 75
+      },
+      {
+        code: "5007804967",
+        name: "KK SUPERMART & SUPERSTORE SDN BHD(JIP5)",
+        category: "KK Supermart",
+        totalSku: 75
+      },
+      {
+        code: "885664042",
+        name: "KK SUPERMART & SUPERSTORE SDN BHD(JIP-SP2)-Jalan Ipoh",
+        category: "KK Supermart",
+        totalSku: 75
+      },
+      {
+        code: "5007805127",
+        name: "KK SUPERMART & SUPERSTORE SDN BHD(JMT)",
+        category: "KK Supermart",
+        totalSku: 75
+      },
+      {
+        code: "885664159",
+        name: "KK SUPERMART & SUPERSTORE SDN BHD(JPH-SP2)-Jalan Pahang",
+        category: "KK Supermart",
+        totalSku: 75
+      },
+      {
+        code: "885664047",
+        name: "KK SUPERMART & SUPERSTORE SDN BHD(JRL-SP2)-Jalan Raja Laut",
+        category: "KK Supermart",
+        totalSku: 75
+      },
+      {
+        code: "885664233",
+        name: "KK SUPERMART & SUPERSTORE SDN BHD(JSE2-SP2)-Jalan Sentul 2",
+        category: "KK Supermart",
+        totalSku: 75
+      },
+      {
+        code: "885664048",
+        name: "KK SUPERMART & SUPERSTORE SDN BHD(JSE-SP2)-Jalan Sentul",
+        category: "KK Supermart",
+        totalSku: 75
+      },
+      {
+        code: "885666516",
+        name: "KK SUPERMART & SUPERSTORE SDN BHD(SCT-SP2)-SETAPAK CENTRAL",
+        category: "KK Supermart",
+        totalSku: 75
+      },
+      {
+        code: "885664389",
+        name: "KK SUPERMART & SUPERSTORE SDN BHD(SDS-SP2)-SENTUL, DATO SENU",
+        category: "KK Supermart",
+        totalSku: 75
+      },
+      {
+        code: "885664226",
+        name: "KK SUPERMART & SUPERSTORE SDN BHD(SDT-SP2)-Solaris, Dutamas",
+        category: "KK Supermart",
+        totalSku: 75
+      },
+      {
+        code: "885664245",
+        name: "KK SUPERMART & SUPERSTORE SDN BHD(SGB2-SP2)-Segambut 2",
+        category: "KK Supermart",
+        totalSku: 75
+      },
+      {
+        code: "885664247",
+        name: "KK SUPERMART & SUPERSTORE SDN BHD(SGB3-SP2)-Segambut",
+        category: "KK Supermart",
+        totalSku: 75
+      },
+      {
+        code: "885664201",
+        name: "KK SUPERMART & SUPERSTORE SDN BHD(SGB-SP2)-Segambut SPPK",
+        category: "KK Supermart",
+        totalSku: 75
+      },
+      {
+        code: "885664382",
+        name: "KK SUPERMART & SUPERSTORE SDN BHD(SHD-SP2)-SRI HARTAMAS, PLAZA DAMAS",
+        category: "KK Supermart",
+        totalSku: 75
+      },
+      {
+        code: "885664007",
+        name: "KK SUPERMART & SUPERSTORE SDN BHD(SH-SP2)-Sri Hartamas",
+        category: "KK Supermart",
+        totalSku: 75
+      },
+      {
+        code: "885664287",
+        name: "KK SUPERMART & SUPERSTORE SDN BHD(SMK-SP2)-SOLARIS MONT KIARA",
+        category: "KK Supermart",
+        totalSku: 75
+      },
+      {
+        code: "885664381",
+        name: "KK SUPERMART & SUPERSTORE SDN BHD(SPS-SP2)-SENTUL PASAR",
+        category: "KK Supermart",
+        totalSku: 75
+      },
+      {
+        code: "885664195",
+        name: "KK SUPERMART & SUPERSTORE SDN BHD(SUTC-SP2)-Sentul, UTC",
+        category: "KK Supermart",
+        totalSku: 75
+      },
+      {
+        code: "885664429",
+        name: "KK SUPERMART & SUPERSTORE SDN BHD(TAR-SP2)-TUANKU ABDUL RAHMAN",
+        category: "KK Supermart",
+        totalSku: 75
+      },
+      {
+        code: "885664318",
+        name: "KK SUPERMART & SUPERSTORE SDN BHD(VJL-SP2)-VISTANA, JALAN LUMUT",
+        category: "KK Supermart",
+        totalSku: 75
+      },
+      {
+        code: "885670071",
+        name: "KK SUPERMART & SUPERSTORE SN 517 (JIP4)",
+        category: "KK Supermart",
+        totalSku: 75
+      },
+      {
+        code: "5007810043",
+        name: "KK SUPERMART & SUPERSTORES SDN BHD (MOS)",
+        category: "KK Supermart",
+        totalSku: 75
+      },
+      {
+        code: "5007813857",
+        name: "MR D.I.Y (SETAPAK MALL)",
+        category: "MR DIY",
+        totalSku: 2
+      },
+      {
+        code: "5000005629",
+        name: "PANAS EXPRESS",
+        category: "Panas Express",
+        totalSku: 0
+      },
+      {
+        code: "900953",
+        name: "PASARAYA OTK (SENTUL) SDN BHD",
+        category: "OTK",
+        totalSku: 5
+      },
+      {
+        code: "5007779282",
+        name: "POS SHOP MEDAN TUANKU",
+        category: "POS Shop",
+        totalSku: 0
+      },
+      {
+        code: "885707859",
+        name: "the store pudu",
+        category: "The Store",
+        totalSku: 69
+      }
+    ],
+
+    M21954: []
+  }
+};
