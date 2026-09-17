e9c92d11ee7e2642a3c2f386988e2725ad5159b0
mehedialimayon-del <mehedialimayon@gmail.com>
2026-09-18 05:45:15 +0800

    Fix syntax error in APP_CONFIG declaration

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
+'use strict';
+
 window.APP_CONFIG = {
-  BACKEND_URL: 'window.APP_CONFIG = {
   BACKEND_URL: 'https://script.google.com/macros/s/AKfycbzW2BMUGVpNj2VmrSVj9XlZ2nlJm4XA55-M1L_wZqkS9fSfXUQVt3XCOzJhTd7jbSB2/exec'
-};'
 };
