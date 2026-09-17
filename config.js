1940f5bc4a12b57af74a064ae5f5ffb600c8d471
mehedialimayon-del <mehedialimayon@gmail.com>
2026-09-17 22:37:33 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 window.APP_CONFIG = {
+  BACKEND_URL: 'window.APP_CONFIG = {
   BACKEND_URL: 'https://script.google.com/macros/s/AKfycbzW2BMUGVpNj2VmrSVj9XlZ2nlJm4XA55-M1L_wZqkS9fSfXUQVt3XCOzJhTd7jbSB2/exec'
+};'
 };
