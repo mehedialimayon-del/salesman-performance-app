115ce7f59fac753036220a77f4c49c5b2a726182
mehedialimayon-del <mehedialimayon@gmail.com>
2026-09-20 13:10:43 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ffh-final-reference-v13';
+const CACHE='ffh-v14-theme-only';
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request))));
